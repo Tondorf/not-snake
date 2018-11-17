807cf605590f77ead431bd49d4a3f60256ebfab9
Lennart Buhl <git@hackmate.de>
2018-11-17 14:02:18 +0100

    use of globals, splitting into multiple files

---
+++ js/food.js
@@ -0,0 +1,75 @@
+var Food = new Phaser.Class({
+
+    Extends: Phaser.GameObjects.Image,
+
+    initialize:
+
+        function Food(scene, x, y) {
+            Phaser.GameObjects.Image.call(this, scene);
+
+            this.setTexture('food');
+            this.setPosition(x * GRID_SIZE, y * GRID_SIZE);
+            this.setOrigin(0);
+
+            this.total = 0;
+
+            scene.children.add(this);
+        },
+
+    eat: function () {
+        this.total++;
+    }
+
+});
+
+/**
+ * We can place the food anywhere in our GRID_X * GRID_Y grid
+ * *except* on-top of the snake, so we need
+ * to filter those out of the possible food locations.
+ * If there aren't any locations left, they've won!
+ *
+ * @method repositionFood
+ * @return {boolean} true if the food was placed, otherwise false
+ */
+function repositionFood() {
+    //  First create an array that assumes all positions
+    //  are valid for the new piece of food
+
+    //  A Grid we'll use to reposition the food each time it's eaten
+    var testGrid = [];
+
+    for (var y = 0; y < GRID_Y; y++) {
+        testGrid[y] = [];
+
+        for (var x = 0; x < GRID_X; x++) {
+            testGrid[y][x] = true;
+        }
+    }
+
+    snake.updateGrid(testGrid);
+
+    //  Purge out false positions
+    var validLocations = [];
+
+    for (var y = 0; y < GRID_Y; y++) {
+        for (var x = 0; x < GRID_X; x++) {
+            if (testGrid[y][x] === true) {
+                //  Is this position valid for food? If so, add it here ...
+                validLocations.push({x: x, y: y});
+            }
+        }
+    }
+
+    if (validLocations.length > 0) {
+        //  Use the RNG to pick a random food position
+        var pos = Phaser.Math.RND.pick(validLocations);
+
+        //  And place it
+        food.setPosition(pos.x * GRID_SIZE, pos.y * GRID_SIZE);
+
+        return true;
+    }
+    else {
+        return false;
+    }
+}
--- js/globals.js
+++ js/globals.js
@@ -1,4 +1,16 @@
-// Globals
-const X = 1200;
-const Y = 800;
-const BGCOLOR = "#101010";
+// Game Field
+const GRID_SIZE = 16;
+const GRID_X = 75;
+const GRID_Y = 50;
+
+// Game Globals
+const GAME_X = GRID_X * GRID_SIZE;
+const GAME_Y = GRID_Y * GRID_SIZE;
+const BG_COLOR = "#101010";
+const PLAYFIELD_BG_COLOR = '#e1ae15';
+
+//  Direction consts
+const UP = 0;
+const DOWN = 1;
+const LEFT = 2;
+const RIGHT = 3;
