50c9211828269c9e55d1412d85b92bac2378b460
Lennart Buhl <git@hackmate.de>
2018-11-17 12:22:32 +0100

    grid

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
-    width: 1200,
-    height: 800,
+    width: 1366,
+    height: 768,
     // physics: {
     //     default: 'arcade',
     //     arcade: {
@@ -16,14 +16,22 @@ var config = {
 
 };
 
+var grid;
+
 var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image('star', 'image/star.png');
 }
 
 function create ()
 {
+    this.add.image(400, 300, 'star');
+    grid = new Phaser.GameObjects.Grid(
+        x=200, y=200,
+        width=128, height=128,
+        cellWidth=16, cellHeight=16);
 }
 
 function update ()
